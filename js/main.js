b022dab92ceac7b5767f137d9b26ea9032d6c798
FrancescoCellitti <francescocellitti@virgilio.it>
2025-05-30 10:54:21 +0200

    JS-snacks-1 project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//l'utente inserisce due numeri in successione con due prompt, il software stampa il maggiore.
+const number1= Number(prompt('inserire un numero'))
+const number2 = Number(prompt('inserire un secondo numero'))
+if(number1>number2){
+    console.log(number1)
+}else{
+    console.log(number2)
+}
