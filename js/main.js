58017022213691289a81ab976db5753316cb33ca
FrancescoCellitti <francescocellitti@virgilio.it>
2025-05-30 11:01:00 +0200

    third condition added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 //l'utente inserisce due numeri in successione con due prompt, il software stampa il maggiore.
-const number1= Number(prompt('inserire un numero'))
+const number1 = Number(prompt('inserire un numero'))
 const number2 = Number(prompt('inserire un secondo numero'))
-if(number1>number2){
-    console.log(number1)
-}else{
-    console.log(number2)
+if (number1 > number2) {
+    console.log(number1);
+} else if(number2>number1){
+    console.log(number2);
+}else {
+    console.log('i numeri so uguali');
 }
